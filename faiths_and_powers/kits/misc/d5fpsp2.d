c38cca0ad7c936f189b872c68a915c270de51ca9
subtledoctor <bm571@nyu.edu>
2020-03-10 09:32:55 -0400

    0.79.9
    a lot

--- faiths_and_powers/kits/misc/d5fpsp2.d
+++ faiths_and_powers/kits/misc/d5fpsp2.d
@@ -1,18 +1,18 @@
 BEGIN ~D5FPSP2~
 
 IF ~Global("D5FPSPNT","GLOBAL",1)~ THEN BEGIN d5fpsp2
-  SAY ~Choose an ability:~
-  IF ~~ THEN REPLY ~Cast spells spontaneously~ GOTO d5fpsp2_1			//	spontaneous
-  IF ~~ THEN REPLY ~No change, cast spells normally~ GOTO d5fpsp2_2			//	no change
+  SAY @121
+  IF ~~ THEN REPLY @122 GOTO d5fpsp2_1			//	spontaneous
+  IF ~~ THEN REPLY @123 GOTO d5fpsp2_2			//	no change
 END
 
 IF ~~ THEN BEGIN d5fpsp2_1 		// spontaneous
-  SAY ~Cast spells spontaneously?~
-  IF ~~ THEN REPLY ~Confirmed, cast spontaneously.~ DO ~ApplySpellRES("D5FPSPT",Myself)~ EXIT 
-  IF ~~ THEN REPLY ~Reconsider~ GOTO d5fpsp2 
+  SAY @124
+  IF ~~ THEN REPLY @125 DO ~ApplySpellRES("D5FPSPT",Myself)~ EXIT 
+  IF ~~ THEN REPLY @128 GOTO d5fpsp2 
 END
 IF ~~ THEN BEGIN d5fpsp2_2 		// no change
-  SAY ~Cast spells normally?~
-  IF ~~ THEN REPLY ~Confirmed, no change, cast spells normally.~ EXIT 
-  IF ~~ THEN REPLY ~Reconsider~ GOTO d5fpsp2 
+  SAY @126
+  IF ~~ THEN REPLY @127 EXIT 
+  IF ~~ THEN REPLY @128 GOTO d5fpsp2 
 END
